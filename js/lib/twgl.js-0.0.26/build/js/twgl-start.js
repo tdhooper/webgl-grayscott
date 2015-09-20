cd98547e7215d44c0eb2810d7c558f88267c2601
Thomas Hooper <thooper@Macintosh.local>
2015-09-20 17:55:59 +0100

    Load dependencies with bower

--- js/lib/twgl.js-0.0.26/build/js/twgl-start.js
+++
@@ -1,8 +0,0 @@
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    } else {
-        root.twgl = factory();
-    }
-}(this, function () {
-
